25da60fe27926165ebc629ec44fd9d1659120875
suemurakenichi <103973713+suemurakenichi@users.noreply.github.com>
2022-10-11 10:11:55 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,2 +1,3 @@
 war who65='world';
-function sayhello65(){alert('Hello,'+who65+'!');}function
+function sayhello65(){alert('Hello,'+who65+'!');}function taro65(){who65='太郎';
+function hanako65(){who65='花子';}

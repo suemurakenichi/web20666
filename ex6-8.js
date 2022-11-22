2aa46083a4a30732e5f7a2777121c295fddd5674
suemurakenichi <103973713+suemurakenichi@users.noreply.github.com>
2022-11-22 10:44:35 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,5 +1,5 @@
 function sayhello68() {
-alert('Hello, ' + document.getElementById(‘who68').innerHTML + '!');
+alert('Hello,'+document.getElementById('who68').innerHTML+'!');
 }
 function taro68() {
 document.getElementById(‘who68').innerHTML = '太郎'; }

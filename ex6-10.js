6d692f930f20a2eabbf4abd33e8a7753d6339392
suemurakenichi <103973713+suemurakenichi@users.noreply.github.com>
2022-12-20 10:38:18 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('logo').src='./image/josaidaigakurogo.png';}
-function imgchange2(){document.getElementById('logo').src='./image/josaidaigakurogo.png';}
+function imgchange1(){document.getElementById('logo').src='./image/城西ロゴNo.2.png';}
+function imgchange2(){document.getElementById('logo').src='./image/城西ロゴNo.1.png';}

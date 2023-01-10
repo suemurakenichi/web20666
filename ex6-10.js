ecb8fcd845e1e7faa51820dde889604f93fdb964
suemurakenichi <103973713+suemurakenichi@users.noreply.github.com>
2023-01-10 09:34:25 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('logo').src='./image/城西ロゴNo.2.png';}
-function imgchange2(){document.getElementById('logo').src='./image/城西ロゴNo.1.png';}
+function imgchange1(){document.getElementById('logo').src='./image/城西ロゴNo.1.png';}
+function imgchange2(){document.getElementById('logo').src='./image/城西ロゴNo.2.png';}
